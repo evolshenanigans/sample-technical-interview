2c5e743243deb471d7fc031ee72d5343fb9f45f7
Gustavo Joshua Gutierrez <51761125+evolshenanigans@users.noreply.github.com>
2023-12-02 05:13:15 +0000

    first commit. app working.

--- Gemfile
+++ Gemfile
@@ -2,4 +2,8 @@ source "https://rubygems.org"
 
 ruby "3.2.1"
 
+gem "sinatra"
+gem "sinatra-contrib"
 gem "activesupport", "~> 7.1"
+gem "puma", "~> 5.0"
+gem "http"
--- birthday_cake.rb
+++ birthday_cake.rb
@@ -18,8 +18,10 @@ Blowing out candles...
 Birthday cake with 10 blown out candles
 
 =end
-
+require "sinatra"
 require "active_support"
+require "sinatra/reloader"
+require "http"
 
 class BirthdayCake
   attr_accessor :age, :lit
@@ -38,7 +40,7 @@ class BirthdayCake
   end
 
   def greet
-    "Happy #{self.age.ordinalize} Birdday!"
+    "Happy #{self.age} Birthday!"
   end
 
   def sing
@@ -49,17 +51,35 @@ class BirthdayCake
     "Birthday cake with #{self.age} #{candles_status} candles"
   end
 
+  def light_candles
+    if self.lit == true
+      return "Already lit"
+    else
+      self.lit = true
+      return "lit"
+    end
+  end
+
+  def blow_out_candles
+    if self.lit == false
+      return "Already blown out"
+    else
+      self.lit = false
+      return "blown out"
+    end 
+  end
+
   def BirthdayCake.celebrate(age)
-    birthday_cake = Birthdaycake.new(age)
+    birthday_cake = BirthdayCake.new(age)
     puts birthday_cake.greet
     puts birthday_cake.to_s
     puts "Lighting candles..."
     birthday_cake.light_candles
     puts birthday_cake.to_s
     puts "Singing happy birthday..."
-    puts birthday_cake.sign
+    puts birthday_cake.sing
     puts "Blowing out candles..."
-    birth_cake.blow_out_candles
+    birthday_cake.blow_out_candles
     puts birthday_cake.to_s
 
     birthday_cake
